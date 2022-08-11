4521a73ebc8c53964a0c09798a1fbacdce4b43aa
Priscille Louis-rose <priscille.louisrose@gmail.com>
2022-08-11 20:42:44 +0200

    lab day 4 extra done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+//list the movies
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then(moviesFromDB => {
+        // console.log(movies)
+        res.render('movies', {movies: moviesFromDB})
+    })
+    .catch((err) => console.log('Error while getting the movies from the DB', err))
+})
+
+
+//display movie details 
+router.get('/movies/:movieId', (req, res, next) => {
+    const {movieId} = req.params
+    Movie.findById(movieId)
+    .then(movie => {
+        console.log(movie)
+        res.render('movieDetails', {movie})
+    })
+    .catch((err) => console.log('Error while getting the movie details from the DB', err))
+})
 
 module.exports = router;
